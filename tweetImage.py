637716bd3bc705ef5c66d2c73cef69899e54c0ac
Tenkoni <caliminion@gmail.com>
2020-02-26 17:46:48 -0600

    Adding function to download images from list of image urls

--- tweetImage.py
+++ tweetImage.py
@@ -1,4 +1,14 @@
 import twint
+import requests
+import shutil
+
+def downloadImages(listURL, path):
+	for tweet in listURL:
+		for imageURL in tweet:
+			r = requests.get(imageURL, stream = True)
+			with open (imageURL.split('/')[4], 'wb') as image: #split to save image with twitter hashed name
+				shutil.copyfileobj(r.raw, image)
+			del r
 
 ##Configuring twint to scrap what we want
 c = twint.Config()
@@ -10,6 +20,10 @@ c.Media = True
 ##scrap the user favourites
 twint.run.Favorites(c)
 
-#store the image url of each tweet into a list
-imagesURL = [i.photos for i in twint.output.tweets_list]
+#store the image url of each tweet into a list if theres an image
+imagesURL = [i.photos for i in twint.output.tweets_list if i.photos]
+
+print("DOWNLOADING IMAGES!!")
+downloadImages(imagesURL, 'path')
+
 
